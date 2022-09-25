a04a849e5295d8c98a486fac16207727623d222f
Prithivi Maruthachalam <prithivimaruthachalam@gmail.com>
2022-09-25 14:26:47 +0530

    full screen windowing support

--- src/Application.cpp
+++ src/Application.cpp
@@ -3,48 +3,77 @@
 #include<iostream>
 using namespace std;
 
+#define LOW_RES_WIDTH 640
+#define LOW_RES_HEIGHT 480
+
 void glfwErrorCallback(int error, const char* description) {
-    fprintf(stderr, "Error: %d | %s\n", error, description);
+	fprintf(stderr, "Error: %d | %s\n", error, description);
 }
 
 int main(void) {
-    // init glfw
-    if(glfwInit() == GLFW_FALSE){
-        cout << "GLFW initialization failed" << endl;
-        return EXIT_FAILURE;
-    }
+	// init glfw
+	if(glfwInit() == GLFW_FALSE){
+		cout << "GLFW initialization failed" << endl;
+		return EXIT_FAILURE;
+	}
 
-    // set error callback function for glfw
-    glfwSetErrorCallback(glfwErrorCallback);
+	// set error callback function for glfw
+	glfwSetErrorCallback(glfwErrorCallback);
+	
+	cout << "Starting window creation..." << endl;
 
-    // create a window and context
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Let's teach me openGL", NULL, NULL);
-    if(window == NULL){
-        cout << "Window or Context creation failed" << endl;
-        glfwTerminate();
-        return EXIT_FAILURE;
-    }
+	// get primary monitor
+	
+	// height and width for window
+	int width = LOW_RES_WIDTH;
+	int height = LOW_RES_WIDTH;
 
-    // make this the current context
-    glfwMakeContextCurrent(window);
+	// reference to primary monitor
+	GLFWmonitor* primaryMonitor = NULL;
 
-    // WINDOW LOOP
-    while(!glfwWindowShouldClose(window)){
-        // clear the screen
-        glClear(GL_COLOR_BUFFER_BIT);
+	// resize for non-dev version
+	#ifndef WINDOW_DEV
+		primaryMonitor = glfwGetPrimaryMonitor();
+		
+		const GLFWvidmode* videomode = glfwGetVideoMode(primaryMonitor);
+		width = videomode->width;
+		height = videomode->height;
+	#endif
+	
+	// create a window and context
+	GLFWwindow* window = glfwCreateWindow(width, height, "Let's teach me openGL", primaryMonitor, NULL);
+	if(window == NULL){
+		cout << "Window or Context creation failed" << endl;
+		glfwTerminate();
+		return EXIT_FAILURE;
+	}
 
-        // swap front and back buffers
-        glfwSwapBuffers(window);
+	// make this the current context
+	glfwMakeContextCurrent(window);
 
-        // poll for events and process them
-        glfwPollEvents();
-    }
+	// WINDOW LOOP
+	while(!glfwWindowShouldClose(window)){
+		// clear the screen
+		glClear(GL_COLOR_BUFFER_BIT);
 
-    // destroy window and context
-    glfwDestroyWindow(window);
+	glBegin(GL_TRIANGLES);	// specify that we're drawing triangles
+	glVertex2f(0.5f, 0.5f);
+	glVertex2f(-0.5f, 0.5f);
+	glVertex2f(0, -0.5f);
+	glEnd();	// end vertex specification
 
-    // terminate glfw
-    glfwTerminate();
+		// swap front and back buffers
+		glfwSwapBuffers(window);
 
-    return EXIT_SUCCESS;
+		// poll for events and process them
+		glfwPollEvents();
+	}
+
+	// destroy window and context
+	glfwDestroyWindow(window);
+
+	// terminate glfw
+	glfwTerminate();
+
+	return EXIT_SUCCESS;
 }
